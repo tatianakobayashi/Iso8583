3010ce765ff38f5a674b6653f250a675ebf6f78d
tatianakobayashi <tatianakobayashi@hotmail.com>
2019-07-29 12:27:08 -0400

    Total transactions

--- iso-8583-test/src/server/ServerStatistics.java
+++ iso-8583-test/src/server/ServerStatistics.java
@@ -5,23 +5,30 @@ import java.util.HashMap;
 
 public class ServerStatistics {
 	private HashMap<String, Integer> transactionsByThread = new HashMap<String, Integer>();
-	
+
 	public void putTransactionsByThread(String threadName, Integer connections) {
 		transactionsByThread.put(threadName, connections);
 	}
-	
-	private float getMeanNumberOfTransactions() {
+
+	private int getTotalTransactions() {
 		ArrayList<Integer> numberOfTransactions = (ArrayList<Integer>) transactionsByThread.values();
-		
+
 		int sum = 0;
 		for (Integer transactions : numberOfTransactions) {
 			sum += transactions;
 		}
-		
-		return sum/numberOfTransactions.size();
+
+		return sum;
 	}
-	
+
+	private float getMeanNumberOfTransactions() {
+		ArrayList<Integer> numberOfTransactions = (ArrayList<Integer>) transactionsByThread.values();
+		
+		return getTotalTransactions() / numberOfTransactions.size();
+	}
+
 	public void printStatistics() {
-		System.out.println("Média de transações por thread: " + getMeanNumberOfTransactions());
+		System.out.println("Média de transações por conexão: " + getMeanNumberOfTransactions());
+		System.out.println("Total de transações: " + getTotalTransactions());
 	}
 }
